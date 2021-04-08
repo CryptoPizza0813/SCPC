c2e10296bafbf6ef38dc7e6164a2e9ba47b15635
jhdh0813 <56108428+jhdh0813@users.noreply.github.com>
2021-04-08 18:10:48 +0900

    0408

---
+++ BACKJOON_1463/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+int arr[100001] = {0, };
+
+
+int main()
+{
+    cin >> N;
+
+    int check = 0;
+
+    for(int i = 1; i <= N; i++) {
+        if(arr[i] != 0 && arr[i] )
+
+
+    }
+
+
+
+    return 0;
+
+}
